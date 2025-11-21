0639508cc51ace0cba123077bafd4b540e4a8fdf
Joondeep <Joonsharma06@gmail.com>
2025-11-21 18:28:28 +0530

    Add introductory C program with personal details

---
+++ Lab_1/intro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Name: Joondeep Sharma\n");
+printf("Roll Number: MEB25021\n");
+printf("Branch: Mechanical Engineering\n");
+printf("University: Tezpur University\n");
+}
