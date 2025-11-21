9618ac12bc7a75b9e4cd7849b05aab59fbf9388f
Joondeep <Joonsharma06@gmail.com>
2025-11-21 18:47:17 +0530

    Implement sum calculation from 1 to N

---
+++ Lab_8/question_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int N,count,sum;
+    scanf("%d",&N);
+    sum=0;
+    count = 1;
+    while(count<=N) {
+        sum=sum+count;
+        count++;
+    }
+    printf("sum=%d\n",sum);
+    return 0;
+
+    }
